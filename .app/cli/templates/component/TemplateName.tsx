6e460270eca80df8239937d74e8a35561010af74
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-09-30 17:09:10 +0330

    Add component generator

---
+++ .app/cli/templates/component/TemplateName.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+import { ITemplateNameProps } from './TemplateName.interface';
+import styles from './TemplateName.module.scss';
+
+export const TemplateName: FC<ITemplateNameProps> = () => {
+  return <div className={styles.TemplateName}>TemplateName component</div>;
+};
