f708a8e32cd67a7a16163d6a394848ce84f3ffaa
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-09-30 13:26:03 +0330

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from 'App'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
     <App />
